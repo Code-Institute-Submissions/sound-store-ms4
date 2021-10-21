734aed329699cc64f39683eec997e09a00430911
aaronh1986 <aaronharbourne@gmail.com>
2021-10-21 20:57:48 +0000

    Create products app and migrate to database

---
+++ products/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import Product, Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'category',
+        'name',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('-sku',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
