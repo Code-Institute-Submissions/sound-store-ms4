1eea43367979370fcbfd5449f395b86fbf9ab0cc
aaronh1986 <aaronharbourne@gmail.com>
2021-10-22 14:39:57 +0000

    Add add_to_bag in views and test it works by using a print statement to print cart to terminal

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def view_bag(request):
     """View which will show selected items on a shopping cart page"""
     return render(request, 'cart/cart.html')
+
+
+def add_to_bag(request, item_id):
+    """Add amount of a product to the shopping cart"""
+    
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    # Check if there is a 'bag' variable stored in the session, if not then make one
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+        
+    else:
+        bag[item_id] = quantity
+        
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
