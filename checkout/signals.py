27474b21dc37e569999b15b398912a4e1d4bc350
aaronh1986 <aaronharbourne@gmail.com>
2021-10-23 10:59:11 +0000

    Update checkout admin and create a forms.py file

---
+++ checkout/signals.py
@@ -0,0 +1,21 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Check for instance of an order sent by OrderLineItem, and check if it's a news
+    order or an update to pre-existing one
+    """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_delete(sender, instance, **kwargs):
+    """
+    Check for a deletion and update accordingly
+    """
+    instance.order.update_total()
