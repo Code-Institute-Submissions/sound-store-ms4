2111534dc4eeab6fc8b1141356a8bed732c44f74
aaronh1986 <aaronharbourne@gmail.com>
2021-10-23 11:44:09 +0000

    Complete checkout.html pending implementation of stripe for payments

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    # Ensures user cannot access checkout by typing /checkout in url
+    if not bag:
+        messages.error(request, "You haven't added anything to your cart just yet")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
