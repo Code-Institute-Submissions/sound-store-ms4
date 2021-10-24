467b66073b9dd40c420b3ac5311e448d2b72c20c
aaronh1986 <aaronharbourne@gmail.com>
2021-10-24 12:55:52 +0000

    Add checkout form caching

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -48,10 +48,24 @@ form.addEventListener('submit', function(ev) {
     $('#submit-button').attr('disabled', true);
     $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
+    
+    // For handling cached data, i.e address
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    var url = '/checkout/cache_checkout_data/';
+
+    
+$.post(url, postData).done(function() {
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
             billing_details: {
+                
                 phone: $.trim(form.phone_number.value),
                 email: $.trim(form.email.value),
                 address:{
@@ -60,21 +74,21 @@ form.addEventListener('submit', function(ev) {
                     city: $.trim(form.town_or_city.value),
                     country: $.trim(form.country.value),
                     state: $.trim(form.county.value),
+                }
             }
-        }
-    },
-    shipping: {
-        name: $.trim(form.full_name.value),
-        phone: $.trim(form.phone_number.value),
-        address: {
-            line1: $.trim(form.street_address1.value),
-            line2: $.trim(form.street_address2.value),
-            city: $.trim(form.town_or_city.value),
-            country: $.trim(form.country.value),
-            postal_code: $.trim(form.postcode.value),
-            state: $.trim(form.county.value),
-        }
-    },
+        },
+        shipping: {
+            name: $.trim(form.full_name.value),
+            phone: $.trim(form.phone_number.value),
+            address: {
+                line1: $.trim(form.street_address1.value),
+                line2: $.trim(form.street_address2.value),
+                city: $.trim(form.town_or_city.value),
+                country: $.trim(form.country.value),
+                postal_code: $.trim(form.postcode.value),
+                state: $.trim(form.county.value),
+            }
+        },
     }).then(function(result) {
         if (result.error) {
             var errorDiv = document.getElementById('card-errors');
@@ -94,4 +108,8 @@ form.addEventListener('submit', function(ev) {
             }
         }
     });
+}).fail(function () {
+    // Reload page if a fail occurs
+    location.reload();
+})
 });
