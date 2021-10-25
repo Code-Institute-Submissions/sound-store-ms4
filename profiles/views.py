56b795d97d0d766245b39fc3c4165103d4e09af7
aaronh1986 <aaronharbourne@gmail.com>
2021-10-25 11:17:25 +0000

    Update profiles view

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
@@ -8,9 +10,22 @@ def profile(request):
     The user's profile
     """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'User details updated')
+    
+    form = UserProfileForm(instance=profile)
+    # User's order history
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True,
     }
 
     return render(request, template, context)
