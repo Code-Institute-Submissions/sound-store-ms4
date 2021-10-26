c87af0d608328cf0bc1255bd9e1fdd34acb2c7dd
aaronh1986 <aaronharbourne@gmail.com>
2021-10-26 20:31:43 +0000

    Add view and template to render edit blog form

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.blog_post_page, name='blog_post_page'),
     path('upload_blog/', views.upload_blog, name='upload_blog'),
+    path('edit_blog/<int:blog_id>', views.edit_blog, name='edit_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from .models import BlogPost
 from .forms import BlogForm
@@ -15,6 +15,7 @@ def blog_post_page(request):
 
 
 def upload_blog(request):
+    """Upload a blog to the database"""
     if request.method == 'POST':
         form = BlogForm(request.POST, request.FILES)
         if form.is_valid():
@@ -30,6 +31,22 @@ def upload_blog(request):
 
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def edit_blog(request, blog_id):
+    """Edit a blog"""
+    blog = get_object_or_404(BlogPost, pk=blog_id)
+    form = BlogForm(instance=blog)
+    messages.info(request, f'Updating {blog.blog_title}')
+
+    template = 'blog/edit_blog.html'
+
+    context = {
+        'form': form,
+        'blog': blog,
     }
 
     return render(request, template, context)
