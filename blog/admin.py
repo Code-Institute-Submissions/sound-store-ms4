fab0bfd38f4bf8fc0608a69aca0a6af7e847787a
aaronh1986 <aaronharbourne@gmail.com>
2021-10-26 12:23:05 +0000

    Update admin.py for blog app

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 from .models import BlogPost
 
 
-admin.site.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = (
+        'blog_created',
+        'blog_title',
+        'blog_author',
+        'blog_updated'
+    )
+
+    list_filter = ('blog_created',)
+
+    search_fields = ['blog_title', 'blog_created']
+
+    ordering = ['-blog_created']
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
