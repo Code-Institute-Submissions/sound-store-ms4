339ac48ab0e229ffdbffa83b618ec258d36d04c1
aaronh1986 <aaronharbourne@gmail.com>
2021-10-26 19:15:04 +0000

    Create upload blog form and render template for it in browser

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_post_page, name='blog_post_page'),
+    path('upload_blog/', views.upload_blog, name='upload_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from .models import BlogPost
-
+from .forms import BlogForm
 
 def blog_post_page(request):
+    """Render the blog in the browser"""
     blogs = BlogPost.objects.all()
 
     context = {
         'blogs': blogs,
     }
     return render(request, 'blog/blog.html', context)
+
+
+def upload_blog(request):
+    form = BlogForm()
+    template = 'blog/upload_blog.html'
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
