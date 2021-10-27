be37074cb1e75fac9faf0e136e6dce11d78f6850
aaronh1986 <aaronharbourne@gmail.com>
2021-10-27 17:01:43 +0000

    Contact app completed and tested by submitting a form and checking it shows in the admin database

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import ContactForm
 
 
 def contact(request):
     """Renders the contact form"""
-    return render(request, 'contact/contact.html')
+    
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thank you for contacting us, we will reply shortly.')
+            return redirect(reverse('home'))
+        else:
+            messages.error(request, 'Contact form not submitted. Pease ensure \
+                all fields are filled out correctly and please try again!')
+    else:
+        form = ContactForm()
+            
+    context = {
+        'form': form,
+    }
+    
+    return render(request, 'contact/contact.html', context)
