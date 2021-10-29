df40517a8646c4a98cce1a339c404f963051049e
aaronh1986 <aaronharbourne@gmail.com>
2021-10-29 16:27:24 +0000

    Set debug to development=true in heroku config vars and set secret key in heroku config vars

--- sound_store/settings.py
+++ sound_store/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ytxe4fcx4v&6(c0vg%!a%#mmvmb9&rkh+nqx@@b6iu0!&e_l8%'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'sound-store-ms-4.herokuapp.com', 'localhost'
